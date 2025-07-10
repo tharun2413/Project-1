746f1626b5dafc16081db84c7bed50e75245e5a0
tharun <tharun247@gmail.com>
2025-07-10 09:28:05 -0500

    I'm commiting these files

---
+++ main.py
@@ -0,0 +1,11 @@
+import pandas as pd
+
+dataset = pd.read_csv("Walmart.csv")
+print("Dataset Loaded")
+
+columns = ["Weekly_Sales", "Holiday_Flag", "Temperature", "Fuel_Price", "CPI", "Unemployment"]
+
+for column in columns:
+    print(f"Mean: {dataset[column].mean}")
+    print(f"Median: {dataset[column].median}")
+    print(f"Mode: {dataset[column].mode()}")
